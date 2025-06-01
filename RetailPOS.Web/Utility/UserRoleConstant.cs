984dee3ff59c8751bc9febf9b44d448c3a1ec63d
vess william <vess1994@gmail.com>
2025-06-01 15:00:35 +0800

    add the role const string

---
+++ RetailPOS.Web/Utility/UserRoleConstant.cs
@@ -0,0 +1,10 @@
+﻿namespace RetailPOS.Web.Utility;
+
+public class UserRoleConstant
+{
+    public const string Admin = "Admin";
+
+    public const string Manager = "Manager";
+
+    public const string Cashier = "Cashier";
+}
