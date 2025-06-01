d9cb4c2f02baa39a2951baf5fd785aa1db75621a
vess william <vess1994@gmail.com>
2025-06-01 22:49:45 +0800

    adstract database seeder

---
+++ RetailPOS.Web/Helper/DatabaseSeeder.cs
@@ -0,0 +1,27 @@
+﻿using RetailPOS.Web.Data;
+
+namespace RetailPOS.Web.Helper;
+
+public class DatabaseSeeder
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly ILogger _logger;
+
+    public DatabaseSeeder(IServiceProvider serviceProvider, ILoggerFactory loggerFactory)
+    {
+        _serviceProvider = serviceProvider;
+        _logger = loggerFactory.CreateLogger<DatabaseSeeder>();
+    }
+
+    public async Task SeedAync()
+    {
+        try
+        {
+            await DbInitializer.InitializeAsync(_serviceProvider);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error while seeding database.");
+        }
+    }
+}
